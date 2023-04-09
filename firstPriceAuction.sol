d00b0188d03576fbd99808c4d3c7ad0a1cbcee7e
a-paulraj <apaulraj@princeton.edu>
2023-04-08 23:37:11 -0400

    First price action draft

--- firstPriceAuction.sol
+++ firstPriceAuction.sol
@@ -4,14 +4,13 @@ pragma solidity ^0.8.0;
 contract Auction {
     address payable public auctionOwner;
     uint public auctionEndTime;
-    uint public highestBid;
+    uint public highestBid; // should this and below be changed to something not public? idk
     address payable public highestBidder;
-    bool public auctionEnded;
+    bool public hasPaid; // default value is false
 
-    mapping(address => uint) public bids;
-    mapping(address => bool) public hasPaid;
+    //mapping(address => uint) public bids;
+    mapping(address => bool) public hasBid; //might not need this, gas prices incentivize not spamming too hard
 
-    event HighestBidIncreased(address bidder, uint amount);
     event AuctionEnded(address winner, uint amount);
 
     constructor(uint biddingTime) {
@@ -21,44 +20,19 @@ contract Auction {
 
     function bid() public payable {
         require(block.timestamp <= auctionEndTime, "Auction has ended");
-        require(msg.value > highestBid, "There is already a higher bid");
-
-        highestBid = msg.value;
-        highestBidder = payable(msg.sender);
-
-        if (highestBid != 0) {
-            // refund previous highest bidder
-            bids[highestBidder] += highestBid;
+        //require(msg.value > highestBid, "There is already a higher bid");
+        require(!hasBid[msg.sender], "Already Bid");
+        if (msg.value > highestBid) {
+            highestBid = msg.value;
+            highestBidder = payable(msg.sender);
         }
-
-        bids[msg.sender] += msg.value;
-        emit HighestBidIncreased(msg.sender, msg.value);
-    }
-
-    function endAuction() public {
-        require(msg.sender == auctionOwner, "You are not the auction owner");
-        require(block.timestamp >= auctionEndTime, "Auction has not ended yet");
-        require(!auctionEnded, "Auction has already ended");
-
-        auctionEnded = true;
-        emit AuctionEnded(highestBidder, highestBid);
     }
 
     function pay() public {
-        require(auctionEnded, "Auction has not ended yet");
-        require(msg.sender == highestBidder, "You are not the highest bidder");
-        require(!hasPaid[msg.sender], "Payment already made");
-
-        hasPaid[msg.sender] = true;
-        payable(msg.sender).transfer(highestBid);
-    }
-
-    function withdraw() public {
         require(block.timestamp >= auctionEndTime, "Auction has not ended yet");
-        require(bids[msg.sender] > 0, "You have no funds to withdraw");
-
-        uint amount = bids[msg.sender];
-        bids[msg.sender] = 0;
-        payable(msg.sender).transfer(amount);
+        require(msg.sender == highestBidder, "You are not the highest bidder");
+        require(!hasPaid, "Payment already made");
+        hasPaid = true;
+        payable(msg.sender).transfer(highestBid);
     }
 }
